f61189c6c836a191ada05c194ad542878ddb4b5c
Jorge Machado <miguelj1992@outlook.com>
2022-10-09 16:44:39 +0100

    Nova Branch teste

--- 01_RestAspNet5ViaVisaulStudio/RestAspNetCoreApi/RestAspNetCoreApi/Controllers/CalculatorController.cs
+++ 01_RestAspNet5ViaVisaulStudio/RestAspNetCoreApi/RestAspNetCoreApi/Controllers/CalculatorController.cs
@@ -17,12 +17,78 @@ namespace RestAspNetCoreApi.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
                 var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
                 return Ok(sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            return BadRequest("Ivalid Input");
+        }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            return BadRequest("Ivalid Input");
+        }
+
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            return BadRequest("Ivalid Input");
+        }
+
+        [HttpGet("square/{firstNumber}")]
+        public IActionResult SquareRoot(string firstNumber)
+        {
+            if (IsNumeric(firstNumber) )
+            {
+
+                var sum = (double)ConvertToDecimal(firstNumber);
+                if (sum >= 0)
+                {
+                    sum = Math.Sqrt(sum);
+                    return Ok(sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+                else
+                    return BadRequest("Invalid Number for SQRT");
+                    
+            }
+
+            return BadRequest("Ivalid Input");
+        }
+
+        // Isto vai ser uma calculadora de serviços
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+
+                var first = (double)ConvertToDecimal(firstNumber);
+                var second = (double)ConvertToDecimal(secondNumber);
+                if (second != 0.0)
+                {
+                    var sum = first/second;
+                    return Ok(sum.ToString("F2", CultureInfo.InvariantCulture));
+                }
+                else
+                    return BadRequest("Division for Zero");
+
             }
 
             return BadRequest("Ivalid Input");
